18743b7977a582cc6a810d65bdd0822dad31ccba
Kit Nicholas Santiago <santiagogoodboykit@gmail.com>
2023-10-05 00:03:51 +0800

    Problem1

---
+++ Abstraction/Problem1.java
@@ -0,0 +1,21 @@
+public class Problem1 {
+    public static void main(String[] args) {
+
+        int[] numbers = {3, 5, 7, 2, 8}; // Define an integer array
+        int sum = 0; // Initialize sum to zero
+
+        // Loop through the array to calculate the sum of elements (complete the code)
+        for (int i = 0; i < numbers.length; i++) { 
+        
+        // Add each element to the sum
+         sum += numbers[i];
+
+        }
+        // Print the sum of the elements
+        System.out.println("The sum of the elements in the array is: " + sum);
+
+        } 
+    }
+    
+
+
